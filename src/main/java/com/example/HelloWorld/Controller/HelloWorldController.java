6b127c362e2fae442bc2e0628e7190212f622309
Tre-Brooks <43584235+Tre-Brooks@users.noreply.github.com>
2022-06-23 16:15:40 -0400

    README.md added

--- src/main/java/com/example/HelloWorld/Controller/HelloWorldController.java
+++ src/main/java/com/example/HelloWorld/Controller/HelloWorldController.java
@@ -1,24 +1,19 @@
 package com.example.HelloWorld.Controller;
 
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.retry.annotation.Retryable;
-import org.springframework.http.HttpStatus;
 import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Recover;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.jayway.jsonpath.JsonPath;
 
 
--- src/main/java/com/example/HelloWorld/HelloWorldApplication.java
+++ src/main/java/com/example/HelloWorld/HelloWorldApplication.java
@@ -1,18 +1,37 @@
 package com.example.HelloWorld;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
 
 
+
 @EnableRetry
 @SpringBootApplication
-public class HelloWorldApplication {
+public class HelloWorldApplication implements ApplicationRunner {
+	
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldApplication.class);;
+	@Value("${person.name}")
+	private String name;
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(HelloWorldApplication.class, args);
 		
 
 	}
+	@Override
+	public void run(ApplicationArguments args) throws Exception
+	{
+		LOGGER.info("Name: " + name);
+	}
+	
+
 
 }
